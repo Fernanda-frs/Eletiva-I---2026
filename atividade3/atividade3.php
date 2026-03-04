2f651907da256f674c3a410b6078e0a0f6d5ab66
fernanda-frs <fer.nan.dinha20022000@gmail.com>
2026-03-03 21:59:34 -0300

    atividade03

--- atividade3/atividade3.php
+++ atividade3/atividade3.php
@@ -2,70 +2,64 @@
 <html lang="pt-BR">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Exemplo Estrutura de Controle e Repetição </title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-  <div class="container py-3">
-    <h1>Exemplo Estrutura de Controle e Repetição </h1>
-    <form method="post">
-      <div class="mb-3">
-        <label for="valor" class="form-label">Informe um valor: </label>
-        <input type="text" id="valor" name="valor" class="form-control" required="">
-      </div>
-      <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
-
-  <?php
+    <div class="container py-3">
+        <h1></h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="valor1" class="form-label">Informe o valor do 1º número:</label>
+                <input type="text" id="valor1" name="valor1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor2" class="form-label">Informe o valor do 2º número:</label>
+                <input type="text" id="valor2" name="valor2" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor3" class="form-label">Informe o valor do 3º número:</label>
+                <input type="text" id="valor3" name="valor3" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor4" class="form-label">Informe o valor do 4º número:</label>
+                <input type="text" id="valor4" name="valor4" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor5" class="form-label">Informe o valor do 5º número:</label>
+                <input type="text" id="valor5" name="valor5" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor6" class="form-label">Informe o valor do 6º número:</label>
+                <input type="text" id="valor6" name="valor6" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="valor7" class="form-label">Informe o valor do 7º número:</label>
+                <input type="text" id="valor7" name="valor7" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form> 
+        <?php
       if ($_SERVER['REQUEST_METHOD'] == "POST"){
-        $valor= $_POST['valor'];
+        $valor1= $_POST['valor'];
+        $valor2= $_POST['valor'];
+        $valor3= $_POST['valor'];
+        $valor4= $_POST['valor'];
+        $valor5= $_POST['valor'];
+        $valor6= $_POST['valor'];
+        $valor7= $_POST['valor'];
         
-        /*if($valor == "+")
-          echo "<p>Sinal de soma!</p>";
-        elseif($valor == "-")
-          echo"<p>Sinal de subtração!</p>";
-        elseif($valor == "*"){
-          echo"<p>Sinal de multiplicação!</p>";
+
         }
-        elseif($valor == "/")
-          echo "<p>Sinal de divisão!</p>";
-        else {
-          echo "<p>Sinal inválido!</p>";
-        }*/ 
+        ?>
 
-        /*switch($valor){
-          case "+":
-            echo "<p>Sinal de soma!</p>";
-            break;
-          case "-":
-            echo"<p>Sinal de subtração!</p>";
-            break;
-          case "*":
-            echo"<p>Sinal de multiplicação!</p>";
-            break;
-          case "/":
-            echo "<p>Sinal de divisão!</p>";
-            break;
-          default:
-            echo "<p>Sinal Inválido!</p>"; 
-            break;
-        }*/
-
-        for($i =1; $i <=10; $i++){
-          $resultado = $valor * $i;
-          echo"<p> $valor * $i = $resultado</p>";
-        }
-        
-      }
-
-  ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
-      crossorigin="anonymous"></script>
-  </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+            crossorigin="anonymous"></script>
+    </div>
 </body>
 
 </html>
