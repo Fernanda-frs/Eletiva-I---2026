147f823889ee3d6ee3dd2024148d85d37b25b9c5
fernanda-frs <fer.nan.dinha20022000@gmail.com>
2026-03-24 21:39:05 -0300

    aula 24 03 projeto

---
+++ Projeto/consultar_categoria.php
@@ -0,0 +1,13 @@
+<?php
+    require_once('cabecalho.php');
+?>
+<h1>Consultar categoria</h1>
+<form method="post">
+<div class="mb-3">
+              <p> <strong>Descrição: </strong> Descrição</p>
+            </div>
+    <button type="submit" class="btn btn-danger">Excluir</button>
+
+<?php
+    require_once('rodape.php');
+?>
