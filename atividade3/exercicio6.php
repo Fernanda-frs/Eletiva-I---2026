0d642337af18573266939c6ab020ac70489d73d7
Fernanda-frs <fer.nan.dinha20022000@gmail.com>
2026-03-30 14:50:02 -0300

    finalizado exe6

--- atividade3/exercicio6.php
+++ atividade3/exercicio6.php
@@ -21,13 +21,11 @@
     if($_SERVER['REQUEST_METHOD'] == "POST"){
         $valor1 = $_POST['valor1'];
         
-        for($i = 1; $i <= $valor1; $i++)
-        {
-            echo
+        for($i = 1; $i <= $valor1; $i++) {
+            echo "<p> Número: $i</p>";
         }
      
-     }
-    
+    }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
