7cc45f6675860bc3a8346343443f2e4fe7f5b02e
Fernanda-frs <fer.nan.dinha20022000@gmail.com>
2026-06-07 09:41:38 -0300

    cabecalho

---
+++ ProjetoP2/cabecalho.php
@@ -0,0 +1,45 @@
+<ul class="navbar-nav me-auto">
+
+    <li class="nav-item">
+        <a class="nav-link" href="principal.php">Início</a>
+    </li>
+
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle"
+           data-bs-toggle="dropdown">
+           Cadastros
+        </a>
+
+        <ul class="dropdown-menu">
+
+            <li>
+                <a class="dropdown-item"
+                   href="alunos.php">
+                   Alunos
+                </a>
+            </li>
+
+            <li>
+                <a class="dropdown-item"
+                   href="professores.php">
+                   Professores
+                </a>
+            </li>
+
+            <li>
+                <a class="dropdown-item"
+                   href="planos.php">
+                   Planos
+                </a>
+            </li>
+
+        </ul>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="logout.php">
+            Sair
+        </a>
+    </li>
+
+</ul>
